af03bd87abb7b6d4d44c677b3ce1ba266a1bba71
rat <radu.talmazan@tuwien.ac.at>
2023-01-23 15:14:58 +0100

    fixed issues with metal atom recognition

--- inputparser.py
+++ inputparser.py
@@ -61,7 +61,7 @@ class XYZ:
         df = df.groupby(['Ion']).max()
         self.metalRadius = df.to_dict().get('Ionic Radius')
 
-        self.metalList = ['LI', 'BE', 'F ', 'NA', 'MG', 'AL', 'SI', 'CL', 'K', 'CA', 'SC', 'TI', 'V', 'CR', 'MN', 'FE',
+        self.metalList = ['LI', 'BE', 'NA', 'MG', 'AL', 'SI', 'K', 'CA', 'SC', 'TI', 'V', 'CR', 'MN', 'FE',
                           'CO', 'NI', 'CU', 'ZN',
                           'GA', 'GE', 'AS', 'SE', 'BR', 'RB', 'SR', 'Y', 'ZR', 'NB', 'MO', 'TC', 'RU', 'RH', 'PD', 'AG',
                           'CD', 'IN', 'SN', 'SB',
@@ -69,7 +69,7 @@ class XYZ:
                           'TM', 'YB', 'YB', 'LU', 'HF',
                           'TA', 'W', 'RE', 'OS', 'IR', 'PT', 'AU', 'HG', 'TL', 'PB', 'BI', 'PO', 'AT', 'FR', 'RA', 'AC',
                           'TH', 'PA', 'U', 'NP',
-                          'PU', 'AM', 'CM', 'BK', 'CF', 'ES', 'FM', 'MD', 'NO', 'LR']  # Metals supported by MCPB
+                          'PU', 'AM', 'CM', 'BK', 'CF', 'ES', 'FM', 'MD', 'NO', 'LR']  # Metals supported by MCPB, F and CL were moved
         self.metals = []
         self.ligands = []
         self.filenames = []
