53b1432b0fc752b432d14834f64861f4a01cf87b
rat <radu.talmazan@tuwien.ac.at>
2023-01-24 12:57:33 +0100

    fix for tleap generation

--- ConfGen.py
+++ ConfGen.py
@@ -301,7 +301,7 @@ Calculations will be set up in:
             self.amber = amberInterface(self.MCPB)
 
         if not self.hasMetal:
-            self.amber.tleapNoMetalSolv(self, self.MCPB, 'A') # change to get values from ligands
+            self.amber.tleapNoMetalSolv(self.MCPB, 'A') # change to get values from ligands
         self.amber.tleapChecker(self.MCPB)
         self.status = self.amber.runTleap()
 
