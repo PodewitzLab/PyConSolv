757c415691d58ade83280c7b4973f2c8aa09b8b5
rat <radu.talmazan@tuwien.ac.at>
2023-03-31 17:44:46 +0200

    removed Boron

--- src/PyConSolv/ConfGen.py
+++ src/PyConSolv/ConfGen.py
@@ -77,7 +77,7 @@ class PyConSolv:
         self.solventParamPath = None
         self.version = '0.2.1.7'
         self.metals = ['LI', 'BE', 'NA', 'MG', 'AL', 'SI', 'K', 'CA', 'SC', 'TI', 'V', 'CR', 'MN', 'FE',
-                       'CO', 'NI', 'CU', 'ZN', 'B',
+                       'CO', 'NI', 'CU', 'ZN',
                        'GA', 'GE', 'AS', 'SE', 'BR', 'RB', 'SR', 'Y', 'ZR', 'NB', 'MO', 'TC', 'RU', 'RH', 'PD', 'AG',
                        'CD', 'IN', 'SN', 'SB',
                        'TE', 'CS', 'BA', 'LA', 'CE', 'PR', 'ND', 'PM', 'SM', 'EU', 'GD', 'TB', 'DY', 'HO', 'ER',
