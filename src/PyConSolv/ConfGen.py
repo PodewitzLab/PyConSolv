e4c8a271e22a6646ace2c9ff0b8f91adb9059ba1
rat <radu.talmazan@tuwien.ac.at>
2023-10-25 18:07:49 +0200

    Upped version to 1.0.3

--- src/PyConSolv/ConfGen.py
+++ src/PyConSolv/ConfGen.py
@@ -94,7 +94,7 @@ class PyConSolv:
         self.refrac = None
         self.epsilon = None
         self.solventParamPath = None
-        self.version = '1.0.2'
+        self.version = '1.0.3'
         self.metals = ['LI', 'BE', 'NA', 'MG', 'AL', 'SI', 'K', 'CA', 'SC', 'TI', 'V', 'CR', 'MN', 'FE',
                        'CO', 'NI', 'CU', 'ZN',
                        'GA', 'GE', 'AS', 'SE', 'BR', 'RB', 'SR', 'Y', 'ZR', 'NB', 'MO', 'TC', 'RU', 'RH', 'PD', 'AG',
--- src/PyConSolv/pyconsolv.py
+++ src/PyConSolv/pyconsolv.py
@@ -8,7 +8,7 @@ from PyConSolv.misc.analysis import Analysis
 
 
 def main():
-    ver = '1.0.2'
+    ver = '1.0.3'
     parser = argparse.ArgumentParser(prog = 'PyConSolv', description='Process commandline arguments for PyconSolv')
     parser.add_argument('input', help = 'input file in XYZ format')
     parser.add_argument('-c', '--charge',  nargs='?', default=0, type=int, help = 'charge of the system, default 0')
